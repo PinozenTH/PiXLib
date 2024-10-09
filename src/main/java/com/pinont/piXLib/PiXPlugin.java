a570ca769d8e378a26b3ac412cd4693857d338af
pinont <pinozen.thailand@gmail.com>
2024-10-09 17:22:00 +0700

    Editing Events

--- src/main/java/com/pinont/piXLib/PiXPlugin.java
+++ src/main/java/com/pinont/piXLib/PiXPlugin.java
@@ -9,6 +9,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class PiXPlugin {
@@ -18,10 +19,7 @@ public class PiXPlugin {
     public static List<BukkitTask> tasks;
 
     public static List<Listener> listeners = new ArrayList<>();
-
-    public static void addListener(Listener listener) {
-        listeners.add(listener);
-    }
+    public static HashMap<Listener, Boolean> ignoreList = new HashMap<>();
 
     public static Plugin getPlugin() {
         return plugin;
@@ -29,6 +27,11 @@ public class PiXPlugin {
 
     public static void setPlugin(final JavaPlugin plugin) {
         PiXPlugin.plugin = plugin;
+        listeners.addAll(List.of(
+        ));
+        ignoreList.putAll(new HashMap<Listener, Boolean>() {{
+        }});
+
         registerEvents(listeners, plugin);
         new Message(plugin.getName() + "is Enabled!").setLoggerType(LoggerType.INFO).send();
     }
@@ -36,6 +39,9 @@ public class PiXPlugin {
     public static void registerEvents(List<Listener> listener, JavaPlugin plugin) {
         for (Listener l : listener) {
             plugin.getServer().getPluginManager().registerEvents(l, plugin);
+            if (ignoreList.get(l)) {
+                continue;
+            }
             new Message("Registered listener: " + l.getClass().getSimpleName()).setMessageType(MessageType.CONSOLE).send();
         }
         new Message("All listeners registered.").setMessageType(MessageType.CONSOLE).send();
